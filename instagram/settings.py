04726d0a951bcbcfe072ef53df8d0aab21c1dbdb
mzambia70 <mzambia70@gmail.com>
2019-06-25 14:00:04 +0300

    decouple

--- instagram/settings.py
+++ instagram/settings.py
@@ -48,7 +48,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
