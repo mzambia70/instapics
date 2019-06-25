db5c33ae2ed830f20c7efe147c281a33769dc51f
mzambia70 <mzambia70@gmail.com>
2019-06-25 13:57:29 +0300

    decouple

--- instagram/settings.py
+++ instagram/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import django_heroku
 import dj_database_url
+import decouple
 from decouple import config,Csv
 
 # Email configurations remember to install python-decouple
